fcbe519025db1ddfc36a03a06712ce6062dd16bb
Claes Östlund <claesostlund@Mefromhd-MBP18.local>
2018-09-07 22:11:55 +0200

    Improvements.

--- PluginLabTimmar.php
+++ PluginLabTimmar.php
@@ -1,5 +1,87 @@
 <?php
 class PluginLabTimmar{
+  private $settings = null;
+  private $mysql;
+  function __construct($buto) {
+    if($buto){
+      /**¨
+       * ¨Include.
+       */
+      wfPlugin::includeonce('wf/form_v2');
+      wfPlugin::includeonce('wf/array');
+      wfPlugin::includeonce('wf/yml');
+      /**
+       * Enable.
+       */
+      wfPlugin::enable('wf/bootstrap');
+      wfPlugin::enable('form/form_v1');
+      wfPlugin::enable('wf/table');
+      wfPlugin::enable('wf/ajax');
+      wfPlugin::enable('wf/bootstrapjs');
+      wfPlugin::enable('wf/dom');
+      wfPlugin::enable('wf/callbackjson');
+      wfPlugin::enable('eternicode/bootstrapdatepicker');
+      
+      if(!wfUser::hasRole('client')){
+        exit('Role issue.');
+      }
+      /**
+       * MySql.
+       */
+      wfPlugin::includeonce('wf/mysql');
+      $this->mysql =new PluginWfMysql();
+      /**
+       * Layout path.
+       */
+      wfArray::set($GLOBALS, 'sys/layout_path', '/plugin/lab/timmar/layout');
+      /**
+       * Settings.
+       */
+      $this->settings = new PluginWfArray(wfArray::get($GLOBALS, 'sys/settings/plugin_modules/'.wfArray::get($GLOBALS, 'sys/class').'/settings'));
+      //$this->settings = wfPlugin::getPluginSettings('lab/timmar', true);
+    }
+  }
+  private function db_open(){
+    $this->mysql->open($this->settings->get('mysql'));
+  }
+  private function db_lab_timmar_calendar_select(){
+    $sql = $this->getSql('lab_timmar_calendar_select');
+    $this->db_open();
+    $this->mysql->execute($sql->get());
+    $rs = ($this->mysql->getStmtAsArray());
+    return $rs;
+  }
+  private function db_lab_timmar_calendar_select_one($id){
+    $sql = $this->getSql('lab_timmar_calendar_select_one');
+    $sql->setByTag(array('id' => $id));
+    $this->db_open();
+    $this->mysql->execute($sql->get());
+    $rs = new PluginWfArray($this->mysql->getStmtAsArrayOne());
+    return $rs;
+  }
+  private function db_lab_timmar_calendar_update_one($id){
+    $sql = $this->getSql('lab_timmar_calendar_update_one');
+    $sql->setByTag(array('id' => $id));
+    $sql->setByTag(wfRequest::getAll());
+    $this->db_open();
+    $this->mysql->execute($sql->get());
+    return null;
+  }
+  private function db_lab_timmar_calendar_insert_one(){
+    $id = wfCrypt::getUid();
+    $sql = $this->getSql('lab_timmar_calendar_insert_one');
+    $sql->setByTag(array('id' => $id));
+    $this->db_open();
+    $this->mysql->execute($sql->get());
+    return $id;
+  }
+  private function db_lab_timmar_calendar_delete_one($id){
+    $sql = $this->getSql('lab_timmar_calendar_delete_one');
+    $sql->setByTag(array('id' => $id));
+    $this->db_open();
+    $this->mysql->execute($sql->get());
+    return null;
+  }
   private function getReportAsObject($filename, $start = null, $end = null){
     wfPlugin::includeonce('wf/array');
     return new PluginWfArray($this->getReport($filename, $start, $end));
@@ -22,6 +104,9 @@ class PluginLabTimmar{
     $google = new PluginGoogleCalendar(true);
     $google->filename = $data->get('filename');
     $google->init();
+    if($google->failure){
+      return array('success' => false);
+    }
     $times = new PluginWfArray();
     foreach ($google->getAllTimeEvents() as $key => $value) {
       $item = new PluginWfArray($value);
@@ -138,6 +223,7 @@ class PluginLabTimmar{
     $report['data'] = $data->get();
     $report['projects'] = $projects->get();
     $report['time_no_match'] = $time_no_match;
+    $report['success'] = true;
     return $report;
   }
   /**
@@ -153,7 +239,14 @@ class PluginLabTimmar{
   public function widget_report($data){
     wfPlugin::includeonce('wf/array');
     $data = new PluginWfArray($data);
-    $report = $this->getReportAsObject($data->get('data/filename'));
+    $element = $this->getElementCalendar($data->get('data/filename'));
+    wfDocument::renderElement($element->get());
+  }
+  private function getElementCalendar($filename, $start=null, $end=null){
+    $report = $this->getReportAsObject($filename, $start, $end);
+    if(!$report->get('success')){
+      exit("Could not find goolge calender via link $filename.");
+    }
     /**
      * Replace.
      */
@@ -215,7 +308,7 @@ class PluginLabTimmar{
     $element->setByTag(array('list' => $projects), 'projects');
     $element->setByTag(array('table_tbody_tr' => $times_tbody), 'time_no_match');
     $element->setByTag($report->get('calendar'), 'calendar');
-    wfDocument::renderElement($element->get());
+    return $element;
   }
   private function clean($str){
     $str = substr($str, 0, strlen($str)-1);
@@ -244,4 +337,101 @@ class PluginLabTimmar{
   private function hours($minutes){
     return number_format(($minutes)/60, 2);;
   }
+  public function getYml($dir){
+    return new PluginWfYml(__DIR__.'/'.$dir.".yml");
+  }
+  private function getElement($name){
+    return new PluginWfYml(__DIR__."/element/$name.yml");
+  }
+  private function getForm($name){
+    return new PluginWfYml(__DIR__."/form/$name.yml");
+  }
+  private function getSql($key){
+    return new PluginWfYml(__DIR__.'/mysql/sql.yml', $key);
+  }
+  public function page_start(){
+    
+    
+    $page = $this->getYml('page/start');
+    $json = json_encode(array('class' => wfGlobals::get('class')));
+    $page->setByTag(array('json' => 'var app = '.$json));
+    /**
+     * Insert admin layout from theme.
+     */
+    $page = wfDocument::insertAdminLayout($this->settings, 1, $page);
+    /**
+     * 
+     */
+    wfDocument::mergeLayout($page->get());
+    
+  }
+  public function page_list(){
+    $class = wfGlobals::get('class');
+    $rs = $this->db_lab_timmar_calendar_select();
+    $items = array();
+    foreach ($rs as $key => $value) {
+      $id = $value['id'];
+      $item = $this->getElement('list_item');
+      $item->setByTag($value);
+      $item->setByTag(array('onclick' => "PluginWfAjax.load('start', '/$class/view/id/$id');"), 'view');
+      $item->setByTag(array('onclick' => "PluginWfBootstrapjs.modal({id: 'lab_timmar_form', url: '/'+app.class+'/form/id/$id', lable: 'Edit'});"), 'edit');
+      $item->setByTag(array('onclick' => "if(confirm('Are you sure?')){ $.get( 'delete/id/$id', function( data ) { location.reload(); }); }"), 'delete');
+      $items[] = $item->get();
+    }
+    $element = $this->getElement('list');
+    $element->setByTag(array('items' => $items));
+    wfDocument::renderElement($element->get());
+  }
+  public function page_view(){
+    $rs = $this->db_lab_timmar_calendar_select_one(wfRequest::get('id'));
+    $widget = $this->getElement('widget_view');
+    $widget->setByTag($rs->get());
+    wfDocument::renderElement(array($widget->get()));
+  }
+  public function page_form(){
+    $form = $this->getForm('edit');
+    if(wfRequest::get('id')){
+      $rs = $this->db_lab_timmar_calendar_select_one(wfRequest::get('id'));
+      $form->setByTag($rs->get());
+    }else{
+      $form->setByTag(array(), 'rs', true);
+    }
+    $widget = wfDocument::createWidget('form/form_v1', 'render', $form->get());
+    wfDocument::renderElement(array($widget));
+  }
+  public function page_capture(){
+    $form = $this->getForm('edit');
+    $widget = wfDocument::createWidget('form/form_v1', 'capture', $form->get());
+    wfDocument::renderElement(array($widget));
+  }
+  public function page_delete(){
+    $rs = $this->db_lab_timmar_calendar_select_one(wfRequest::get('id'));
+    if($rs->get('id')){
+      $this->db_lab_timmar_calendar_delete_one($rs->get('id'));
+    }
+    exit('delete...');
+  }
+  public function capture(){
+    if(wfRequest::get('id')){
+      $this->db_lab_timmar_calendar_update_one(wfRequest::get('id'));
+    }else{
+      $id = $this->db_lab_timmar_calendar_insert_one();
+      $this->db_lab_timmar_calendar_update_one($id);
+    }
+    return array("location.reload()");
+  }
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
